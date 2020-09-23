4cdbce8bf7aa759a2342d6ba391ccb5a7557810d
Sean Suwanaloet <suwanaloet@wisc.edu>
2020-09-22 22:14:36 -0700

    product to tag fix for double declare

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,33 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-//category can have multple products
-//product can only belong to one category
 Product.belongsTo(Category, {
-  //
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
 });
 
 
 // Categories have many Products
 Category.hasMany(Product, {
-  //
-  foreignKey: 'category_id'
+  foreignKey: 'category_id',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'product_tag',
-  foreignKey: 'product_id'
+  foreignKey: 'product_id',
 })
 
 // Tags belongToMany Products (through ProductTag)
 //tag belongs to many product models?
-Product.belongsToMany(Product, {
+Tag.belongsToMany(Product, {
   through: ProductTag,
-  as: 'product_tag',
-  foreignKey: 'tag_id'
+  foreignKey: 'tag_id',
 })
 
 module.exports = {
